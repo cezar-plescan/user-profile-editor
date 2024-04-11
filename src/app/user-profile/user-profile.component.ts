3d864e8ded70ba51876419ef2714860b5ec915cd
cezar <plescan.cezar@gmail.com>
2024-04-11 12:36:46 +0300

    - created the form layout

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { CommonModule } from "@angular/common";
+import { Component, inject } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from "@angular/common";
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, NgOptimizedImage],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
-
+  protected form = inject(FormBuilder).group({
+    name: new FormControl('Smith', [Validators.required]),
+    email: new FormControl('office@smith.com', [Validators.required, Validators.email]),
+    address: new FormControl('Broadway', [Validators.required]),
+    avatar: new FormControl('')
+  });
 }
